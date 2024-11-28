60363b1220e5d7dbe88457c491a2d5f78df2d462
Johan Sageryd <j@1616.se>
2024-11-28 20:11:33 +0100

    Introduce focus blocks
    Focus blocks is an attempt at offloading the overhead of keeping track
    of what to be focusing on. Define blocks in the evening or the early
    morning, and then use them as a guide throughout the day.
    
    The implementation is a bit hacky, but I think it should be good enough.

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ $ go install github.com/jsageryd/tmux-tomato@latest
 status bar like this:
 
 ```tmux
+set -g status-right-length 200
 set -g status-style bg=colour236,fg=colour246
 
 if-shell -b 'which -s tmux-tomato' {
@@ -42,6 +43,32 @@ $ alias tt=tmux-tomato
 $ tt 9m30s
 ```
 
+### Configure focus blocks (optional)
+Focus blocks is an attempt at offloading the overhead of keeping track of what
+to be focusing on. Define blocks in the evening or the early morning, and then
+use them as a guide throughout the day.
+
+If the file `$HOME/.tmux-tomato/blocks` is present, tmux-tomato will read focus
+blocks from it. Each line in the file represents one block. Example:
+
+```
+09:00 Project A 3h
+10:30 Meeting 30m
+11:30 Lunch 1h
+13:00 Project B 4h
+```
+
+tmux-tomato prints the currently active focus block and/or the next focus block.
+
+- There may be gaps between focus blocks.
+- Focus blocks may overlap. In this case, the overlapped block continues after
+  the overlapping block ends.
+- The focus blocks may not span more than 24 hours.
+- The focus blocks may span over midnight.
+- `□` denotes the first block of the day. `▶` and `▷` denote an upcoming block.
+  `▶` means the next block starts directly after the current block ends. `▷`
+  indicates that there is a gap between the current block and the next block.
+
 ## Examples
 
 ### Normal mode
@@ -49,6 +76,9 @@ $ tt 9m30s
 <img src="img/state-02.png" width="290px">
 <img src="img/state-03.png" width="290px">
 <img src="img/state-04.png" width="290px">
+
+### Focus blocks
+<img src="img/state-focus.png" width="710px">
 
 ### Egg timer
 <img src="img/state-egg.png" width="290px">
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 )
@@ -109,7 +113,299 @@ func main() {
 		fgColor = "color0"
 	}
 
-	fmt.Printf("#[fg=%s,bg=%s] %s %s %s#[default]", fgColor, bgColor, timeLeft, progressStr, curState.icon)
+	blockFGColor := fmt.Sprintf("color%d", 255)
+	blockBGColor := "default"
+
+	nextBlockFGColor := fmt.Sprintf("color%d", 250)
+	nextBlockBGColor := "default"
+
+	hhmmString := func(d time.Duration) string {
+		haveHours := int(d.Hours()) > 0
+		haveMinutes := int(d.Minutes())%60 > 0
+		haveSeconds := int(d.Seconds())%60 > 0
+
+		switch {
+		case haveHours && haveMinutes:
+			return fmt.Sprintf("%dh%dm", int(d.Hours()), int(d.Minutes())%60)
+		case haveHours && !haveMinutes:
+			return fmt.Sprintf("%dh", int(d.Hours()))
+		case !haveHours && haveMinutes:
+			return fmt.Sprintf("%dm", int(d.Minutes())%60)
+		case !haveHours && !haveMinutes && haveSeconds:
+			return fmt.Sprintf("%ds", int(d.Seconds())%60)
+		default:
+			return "0s"
+		}
+	}
+
+	statusStr := fmt.Sprintf(" #[fg=%s,bg=%s] %s %s %s#[default]", fgColor, bgColor, timeLeft, progressStr, curState.icon)
+
+	var blockStr string
+
+	{
+		blocks, err := readBlocks(now)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("error reading blocks file: %v\n", err)
+			os.Exit(1)
+		}
+
+		if len(blocks) > 0 {
+			blocks = linearizeBlocks(blocks)
+
+			midnight := time.Date(blocks[0].Start.Year(), blocks[0].Start.Month(), blocks[0].Start.Day(), 0, 0, 0, 0, blocks[0].Start.Location())
+			offset := blocks[0].Start.Sub(midnight)
+
+			now := now // make now local to avoid affecting now outside of this block
+
+			// In case now crosses over to the next day, add a day to now so that it
+			// matches the block start time in case a block starts on the next day.
+			if now.Day() != now.Add(-offset).Day() && now.Before(blocks[0].Start) {
+				now = now.AddDate(0, 0, 1)
+			}
+
+			var cur, next Block
+
+			if curIdx := slices.IndexFunc(blocks, func(b Block) bool {
+				return b.covers(now)
+			}); curIdx > -1 {
+				cur = blocks[curIdx]
+			}
+
+			var nextIsFirst bool
+
+			if nextIdx := slices.IndexFunc(blocks, func(b Block) bool {
+				return b.Start.After(now)
+			}); nextIdx > -1 {
+				nextIsFirst = nextIdx == 0
+				next = blocks[nextIdx]
+			}
+
+			if next == (Block{}) && len(blocks) > 0 {
+				nextIsFirst = true
+				next = blocks[0]
+				next.Start = next.Start.AddDate(0, 0, 1)
+			}
+
+			if cur != (Block{}) {
+				blockStr = fmt.Sprintf(" #[fg=%s,bg=%s]%s %s", blockFGColor, blockBGColor, cur.Start.Format("15:04"), cur.Desc)
+
+				if cur.Duration > 0 {
+					remainingTime := cur.end().Sub(now)
+					blockStr += fmt.Sprintf(" %s (%s left)", hhmmString(cur.Duration), hhmmString(remainingTime))
+				}
+			}
+
+			if next != (Block{}) {
+				triangle := "▶"
+
+				if cur == (Block{}) || (cur != (Block{}) && next.Start.Sub(cur.end()) > 0) {
+					if nextIsFirst {
+						triangle = "□"
+					} else {
+						triangle = "▷"
+					}
+				}
+
+				blockStr += fmt.Sprintf(" #[fg=%s,bg=%s]%s %s %s", nextBlockFGColor, nextBlockBGColor, triangle, next.Start.Format("15:04"), next.Desc)
+
+				if next.Duration > 0 {
+					blockStr += " " + hhmmString(next.Duration)
+				}
+			}
+
+			if blockStr != "" {
+				statusStr = blockStr + " |" + statusStr[1:]
+			}
+		}
+	}
+
+	fmt.Print(statusStr)
+	fmt.Println()
+}
+
+type Block struct {
+	Start    time.Time
+	Desc     string
+	Duration time.Duration
+}
+
+func (b Block) covers(t time.Time) bool {
+	return t.Equal(b.Start) || (t.After(b.Start) && (b.Duration == 0 || t.Before(b.Start.Add(b.Duration))))
+}
+
+func (b Block) end() time.Time {
+	return b.Start.Add(b.Duration)
+}
+
+func (b Block) String() string {
+	return fmt.Sprintf("%s %s %s", b.Start.Format("15:04"), b.Desc, b.Duration)
+}
+
+func readBlocks(now time.Time) ([]Block, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error finding home directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	blocksFile := filepath.Join(home, ".tmux-tomato", "blocks")
+
+	data, err := os.ReadFile(blocksFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var blocks []Block
+	var lastStart time.Time
+	var lineNo int
+
+	for s := bufio.NewScanner(bytes.NewReader(data)); s.Scan(); {
+		lineNo++
+
+		line := s.Text()
+		if line == "" {
+			continue
+		}
+
+		firstSpace := strings.Index(line, " ")
+		lastSpace := strings.LastIndex(line, " ")
+
+		if firstSpace == -1 || lastSpace == -1 || firstSpace == lastSpace {
+			return nil, fmt.Errorf(`line %d: invalid format, want e.g. "10:00 Coffee break 1h"`, lineNo)
+		}
+
+		startTimeStr := line[:firstSpace]
+		description := line[firstSpace+1 : lastSpace]
+		durationStr := line[lastSpace+1:]
+
+		start, err := time.Parse("15:04", startTimeStr)
+		if err != nil {
+			return nil, fmt.Errorf("line %d: cannot parse %q as an HH:MM timestamp", lineNo, startTimeStr)
+		}
+
+		start = time.Date(
+			now.Year(), now.Month(), now.Day(),
+			start.Hour(), start.Minute(), 0, 0,
+			now.Location(),
+		)
+
+		if start.Equal(lastStart) {
+			return nil, fmt.Errorf("line %d: there is already a block starting at %s", lineNo, start.Format("15:04"))
+		}
+
+		if start.Before(lastStart) {
+			start = start.AddDate(0, 0, 1)
+		}
+
+		lastStart = start
+
+		duration, err := time.ParseDuration(durationStr)
+		if err != nil {
+			return nil, fmt.Errorf("line %d: cannot parse %q as a duration", lineNo, durationStr)
+		}
+
+		blocks = append(blocks, Block{
+			Start:    start,
+			Desc:     description,
+			Duration: duration,
+		})
+	}
+
+	if len(blocks) > 0 {
+		start := blocks[0].Start
+		end := blocks[0].end()
+		for _, b := range blocks[1:] {
+			if b.end().After(end) {
+				end = b.end()
+			}
+		}
+
+		if end.Sub(start) > 24*time.Hour {
+			return nil, fmt.Errorf("blocks may not span more than 24 hours")
+		}
+	}
+
+	return blocks, nil
+}
+
+func linearizeBlocks(blocks []Block) []Block {
+	var linear []Block
+	var stack []Block
+	var last Block
+
+	if len(blocks) == 0 {
+		return nil
+	}
+
+	midnight := blocks[0].Start.Truncate(24 * time.Hour)
+	offset := blocks[0].Start.Sub(midnight)
+
+	for _, cur := range blocks {
+		cur.Start = cur.Start.Add(-offset)
+
+		for len(stack) > 0 {
+			if last.end().After(cur.Start) {
+				break
+			}
+
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			shift := last.end().Sub(top.Start)
+			top.Start = top.Start.Add(shift)
+			top.Duration -= shift
+
+			if top.Duration <= 0 {
+				continue
+			}
+
+			linear = append(linear, top)
+
+			last = top
+		}
+
+		if last.end().After(cur.Start) {
+			stack = append(stack, last)
+		}
+
+		if len(linear) > 0 {
+			if cur.Start.Before(linear[len(linear)-1].end()) {
+				diff := cur.Start.Sub(linear[len(linear)-1].Start)
+				if diff <= 0 {
+					linear = linear[:len(linear)-1]
+				} else if linear[len(linear)-1].Duration > diff {
+					linear[len(linear)-1].Duration = diff
+				}
+			}
+		}
+
+		linear = append(linear, cur)
+
+		last = cur
+	}
+
+	for len(stack) > 0 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		shift := last.end().Sub(top.Start)
+		top.Start = top.Start.Add(shift)
+		top.Duration -= shift
+
+		if top.Duration <= 0 {
+			continue
+		}
+
+		linear = append(linear, top)
+
+		last = top
+	}
+
+	for n := range linear {
+		linear[n].Start = linear[n].Start.Add(offset)
+	}
+
+	return linear
 }
 
 func eggTimer(now time.Time) (timeLeft time.Duration, active bool) {
